e0368cb6302235a448ef8b3dda66c07a65b636cb
AmirAliShokri <amirali.shokri@gmail.com>
2021-02-17 23:19:29 +0330

    Fix Inputs

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ToDoService } from '../Services/to-do.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: ToDoService) {}
+  error: string = '';
+  ngOnInit(): void {}
+  Add(i: string) {
+    if (i.length > 300 || i == '') {
+      this.error = 'Plz Use Valid Input';
+      return;
+    }
+    this.error = '';
+    this.service.Post(i);
   }
-
 }
